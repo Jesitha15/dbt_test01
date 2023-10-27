7d6e414e0a4d3643a85487171e5ca5245a9a9660
Jesitha15 <148961758+Jesitha15@users.noreply.github.com>
2023-10-27 12:00:02 +0530

    Update inter_annual_enterprise_survey.sql

--- models/example/inter_annual_enterprise_survey.sql
+++ models/example/inter_annual_enterprise_survey.sql
@@ -1,16 +1,16 @@
 	with
 {{config(materialized='view')}}
 
-inter-01 as (
+inter01 as (
 SELECT
      SUM(CAST(t2.value AS INT64)) AS Total_Value,
      {{ dbt_utils.star(from=ref('Stage_annual_enterprise_survey01'), except=["id"]) }},
      {{ dbt_utils.star(from=ref('Stage_annaul_enterprise_survey02'), except=["id"]) }}
         
 FROM
-     {{ ref('Stage_annual_enterprise_survey01’) }} AS t2
+     {{ ref('Stage_annual_enterprise_survey02’) }} AS t2
 LEFT JOIN
-     {{ ref('Stage_annual_enterprise_survey01') }} AS t1
+      {{ ref('Stage_annual_enterprise_survey01') }} AS t1
 ON
     t1.id = t2.id
 )
