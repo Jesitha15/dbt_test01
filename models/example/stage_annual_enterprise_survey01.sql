5d7ba5e1226d51d53af2c609b2bfee807a756283
Jesitha15 <148961758+Jesitha15@users.noreply.github.com>
2023-10-27 12:27:21 +0530

    Update stage_annual_enterprise_survey01.sql

--- models/example/stage_annual_enterprise_survey01.sql
+++ models/example/stage_annual_enterprise_survey01.sql
@@ -1,6 +1,6 @@
 With
 tab1 As(
-Select * from  ‘cloudside-academy.dbt_sample_data.annual-enterprise-survey01’
+Select * from  `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`
 ), one AS (
  Select 
    year, value,id, Industry_name_NZSIOC AS industry_name
